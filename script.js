c3598bfe17a9b29db9e15839b32a9bc598e9c344
Joao Caio <jocaio@uol.com.br>
2021-11-23 18:37:15 -0300

    Arronw Functions

---
+++ script.js
@@ -0,0 +1,29 @@
+/*function somar(x, y) {
+    return x + y;
+}
+*/
+/*
+let somar = function(x, y) {
+    return x + y;
+}
+*/
+/*
+let somar = (x ,y) => {
+    return x + y;
+}
+*/
+/*
+// em uma linha
+let somar = (x, y) => x + y;*/
+
+/*
+// Outro exemplo
+let letrasNoNome = (nome) => {
+    return nome.length;
+}
+*/
+let letrasNoNome = (nome) => nome.length;
+
+
+
+console.log( letrasNoNome('Bernardino'));
